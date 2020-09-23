e0744543a556a751676c5324feea26a656c074bc
Ammar Pathan <ammarpathan@ammars-mbp.myfiosgateway.com>
2020-09-23 02:18:35 -0400

    Basic implementation of movies get api and requirements folder

---
+++ source/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+from flask_restx import Resource, Api
+import json
+
+
+app = Flask(__name__)
+api = Api(app)
+f = open('movies.json',)
+movies = json.load(f)
+# movies = [
+#     {
+#         "name": "The Shawshank Redemption",
+#         "id": "1",
+#         "cast": [
+#             "Tim Robbins",
+#             "Morgan Freeman",
+#             "Bob Gunton",
+#             "William Sadler"
+#         ],
+#         "genres": [
+#             "Drama"
+#         ]
+#     },
+#     {
+#         "name": "The Godfather",
+#         "id": "2",
+#         "cast": [
+#             "Marlon Brando",
+#             "Al Pacino",
+#             "James Caan",
+#             "Diane Keaton"
+#         ],
+#         "genres": [
+#             "Crime",
+#             "Drama"
+#         ]
+#     }
+# ]
+
+
+
+@api.route('/movies')
+class TodoSimple(Resource):
+    def get(self):
+        return jsonify(movies)
+
+@api.route('/movies/<string:movie_id>')
+class MovieById(Resource):
+    def get(self, movie_id):
+        out = [x for x in movies if x['id'] == movie_id]
+        print (out)
+
+
+        return jsonify(out)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ source/local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run our development server locally
+
+FLASK_APP=app flask run --host=127.0.0.1 --port=8000
