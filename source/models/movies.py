9f89b6ae4e69f7ebed0e7e9c4aa0ab285fa75bfe
Ammar Pathan <ammarpathan@ammars-mbp.myfiosgateway.com>
2020-09-28 13:48:29 -0400

    Object Id in import

--- source/models/movies.py
+++ source/models/movies.py
@@ -1,6 +1,8 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from db import *
+from bson.objectid import ObjectId
+
 
 db = connect_to_database('movies-db')
 
