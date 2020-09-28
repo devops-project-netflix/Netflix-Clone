7214b1e06da17d4d8f7c62fe73f4f797abd5ab3f
Varun Ojha <vo383@nyu.edu>
2020-09-28 00:59:06 -0400

    Added functioning seeders

--- source/seeds/movies_seeder.py
+++ source/seeds/movies_seeder.py
@@ -1,9 +1,8 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from models.movies import MoviesModel
 import json
 from db import *
-
-db = connect_to_database('movies-db')
 
 class MoviesSeed:
 	def __init__(self):
@@ -11,7 +10,11 @@ class MoviesSeed:
 		self.seed_file = cwd + '\\seeds\\data\\movies.json'
 		
 	def seed(self):
+		print("Movie seed ..")
 		with open(self.seed_file) as json_file:
 			data = json.load(json_file)
-			print(data)
-			print("movies-seeder")
+			for index in range(len(data)):
+				movie = MoviesModel().get({'Title':data[index]['Title']})
+				movie = [doc for doc in movie]
+				if(0 == len(movie)):
+					code = MoviesModel().insert(data[index])
