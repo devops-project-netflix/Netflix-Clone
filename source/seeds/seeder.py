a57050d98287f61ae5bfed29beb77e1343659106
Sujay Lokesh <sl5202@nyu.edu>
2020-10-02 12:34:57 -0400

    commit to merg

--- source/seeds/seeder.py
+++ source/seeds/seeder.py
@@ -1,7 +1,9 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from seeds.movies_seeder import *
+from seeds.categories_seeder import *
 
 def database_seed():
-	print("In seeder")
+	print("Running seeders ..")
 	MoviesSeed().seed()
+	CategoriesSeed().seed()
