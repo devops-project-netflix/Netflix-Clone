a489ca111e21b77c1b72d8dbb354144638314aff
Sujay Lokesh <sl5202@nyu.edu>
2020-11-03 08:19:09 -0500

    editied the file permissions

--- source/scripts/deploy.py
+++ source/scripts/deploy.py
@@ -2,7 +2,7 @@ import subprocess
 
 a = 'devops.pem'
 
-p = subprocess.Popen(['ssh','-i','../../'+a,'ubuntu@ec2-52-3-255-1.compute-1.amazonaws.com'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,universal_newlines=True,bufsize=0)
+p = subprocess.Popen(['ssh','-i','~/'+'devops.pem','ubuntu@ec2-52-3-255-1.compute-1.amazonaws.com'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,universal_newlines=True,bufsize=0)
 
 p.stdin.write("sudo fuser -k 80/tcp\n")
 
