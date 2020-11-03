612d5259dc9f29d1a512bd817b08f54d5cb14b37
Sujay Lokesh <sl5202@nyu.edu>
2020-11-03 08:39:34 -0500

    cleaning up and finished creating CI/CD pipeline

--- source/scripts/deploy.py
+++ source/scripts/deploy.py
@@ -1,6 +1,6 @@
 import subprocess
 
-a = 'devops.pem'
+
 
 p = subprocess.Popen(['ssh','-o','StrictHostKeyChecking=no','-i','devops.pem','ubuntu@ec2-52-3-255-1.compute-1.amazonaws.com'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,universal_newlines=True,bufsize=0)
 
@@ -11,8 +11,6 @@ p.stdin.write("cd Netflix-Clone\n")
 p.stdin.write("git pull\n")
 p.stdin.write("cd source\n")
 p.stdin.write("source /home/ubuntu/myvenv/bin/activate \n")
-
-
 
 p.stdin.write("psudo ./local.sh\n")
 p.stdin.write("logout\n")
