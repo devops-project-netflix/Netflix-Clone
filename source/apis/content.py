b7b5a96272123a363b63212b2c3759417862de79
Varun Ojha <vo383@nyu.edu>
2020-11-08 22:07:00 -0500

    Factored content & mapped to video file upload

--- source/apis/content.py
+++ source/apis/content.py
@@ -2,10 +2,11 @@ from flask import request
 from werkzeug.datastructures import FileStorage
 
 from flask_restx import Resource, fields, Namespace
-from models.categories import CategoriesModel
+from models.content import ContentModel
 from utilities.responses import http_response
 from utilities.storage import upload
 from utilities.validate import allowed_file
+from config import VIDEO_CONTENT_BUCKET
 
 
 api = Namespace('Content', description='upload & transencoding video endpoints')
@@ -26,15 +27,20 @@ parser.add_argument('file', type=FileStorage, location='files')
 class Content(Resource):
 
     @api.expect(parser)
-    @api.doc(responses={201: 'Category Inserted'})
+    @api.doc(responses={201: 'Content Inserted'})
     def post(self):
         file = request.files['file']
         if file.filename == "":
-        	return http_response(422, {"status": "please use mp4 file"})
+        	return http_response(422, {"status": "please use valid mp4 file"})
 
         if file and allowed_file(file.filename):
         	upload(request.files['file'])
+            content = []
+            content['content'] = file.filename
+            content['bucket'] = VIDEO_CONTENT_BUCKET
+            content['status'] = 'PENDING'
+            ContentModel().insert(content)
         else:
-        	return http_response(422, {"status": "please use mp4 file"})
+        	return http_response(422, {"status": "please use valid mp4 file"})
         	
         return http_response(201, {"status": "video content record added"})
