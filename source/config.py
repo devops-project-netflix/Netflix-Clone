c9711413bda6bbfe440c942c7f30d782337a8148
Sujay Lokesh <sl5202@nyu.edu>
2020-11-18 19:51:25 -0500

    REMOVING AWS KEYS

--- source/config.py
+++ source/config.py
@@ -8,8 +8,8 @@ session = boto3.Session()
 credentials = session.get_credentials()
 
 
-S3_KEY                    = "AKIAVD7Q7SPR4CS764YV"
-S3_SECRET                 = "3PccgpU4c2GvtsZigo1L4Hj4TV5v6a0OOBNRKMC/"
+S3_KEY                    = "NEED TO REPLACE"
+S3_SECRET                 = "NEED TO REPLACE WITH SECRETS"
 S3_REGION                 = "us-west-1"
 
 MONGO_CONNECTION_STRING = "mongodb+srv://ammar:abcd123@cluster0.8f754.mongodb.net/movies-db?retryWrites=true&w=majority"
