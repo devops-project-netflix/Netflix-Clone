0908135b3bc0bd52114de1011e774b709404667d
Sujay Lokesh <sl5202@nyu.edu>
2020-11-19 12:08:57 -0500

    made more changes to config

--- source/config.py
+++ source/config.py
@@ -8,8 +8,8 @@ session = boto3.Session()
 credentials = session.get_credentials()
 
 
-S3_KEY                    = AWS_ACCESS_KEY_ID
-S3_SECRET                 = AWS_SECRET_ACCESS_KEY
+S3_KEY                    = os.environ['AWS_ACCESS_KEY_ID']
+S3_SECRET                 = os.environ['AWS_SECRET_ACCESS_KEY']
 S3_REGION                 = "us-east-1"
 
 MONGO_CONNECTION_STRING = "mongodb+srv://ammar:abcd123@cluster0.8f754.mongodb.net/movies-db?retryWrites=true&w=majority"
