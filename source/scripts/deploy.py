bf335f102c7bb235695f73082965af2c7b7d2999
Sujay Lokesh <sl5202@nyu.edu>
2020-11-19 12:34:49 -0500

    finished docker/ lets see if it works

--- source/scripts/deploy.py
+++ source/scripts/deploy.py
@@ -4,15 +4,7 @@ import subprocess
 
 p = subprocess.Popen(['ssh','-o','StrictHostKeyChecking=no','-i','devops.pem','ubuntu@ec2-52-3-255-1.compute-1.amazonaws.com'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,universal_newlines=True,bufsize=0)
 
-p.stdin.write("sudo fuser -k 80/tcp\n")
-
-p.stdin.write("cd Netflix-Clone\n")
-
-p.stdin.write("git pull\n")
-p.stdin.write("cd source\n")
 p.stdin.write("source /home/ubuntu/myvenv/bin/activate \n")
-
-p.stdin.write("psudo ./local.sh\n")
-p.stdin.write("logout\n")
+p.stdin.write("bash final_deployment.sh")
 p.stdin.close()
 print("Done")
