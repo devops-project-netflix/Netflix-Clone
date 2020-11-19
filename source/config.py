218c31b56337e20309758f795d80d63c9ac584f2
Sujay Lokesh <sl5202@nyu.edu>
2020-11-19 11:57:46 -0500

    changes to config.py

--- source/config.py
+++ source/config.py
@@ -4,12 +4,12 @@ import boto3
 import sys
 sys.setrecursionlimit(5000)
 
-session = boto3.Session(profile_name='default')
+session = boto3.Session()
 credentials = session.get_credentials()
 
 
-S3_KEY                    = credentials.access_key
-S3_SECRET                 = credentials.secret_key
+S3_KEY                    = AWS_ACCESS_KEY_ID
+S3_SECRET                 = AWS_SECRET_ACCESS_KEY
 S3_REGION                 = "us-east-1"
 
 MONGO_CONNECTION_STRING = "mongodb+srv://ammar:abcd123@cluster0.8f754.mongodb.net/movies-db?retryWrites=true&w=majority"
